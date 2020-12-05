760a5ab3d85253da3d9b0bbb04d4ba91def37c5e
Maarten Van Coile <maarten.vancoile@me.com>
2020-12-05 22:27:00 +0100

    cleanup

--- 2020/day04/target/debug/deps/day04-0adaab7d14de9971.d
+++
@@ -1,5 +0,0 @@
-/Users/maarten/Documents/Code/adventofcode/2020/day04/target/debug/deps/day04-0adaab7d14de9971.rmeta: src/main.rs
-
-/Users/maarten/Documents/Code/adventofcode/2020/day04/target/debug/deps/day04-0adaab7d14de9971.d: src/main.rs
-
-src/main.rs:
--- 2020/day04/target/debug/deps/day04-57736d1bc1164831.d
+++
@@ -1,5 +0,0 @@
-/Users/maarten/Documents/Code/adventofcode/2020/day04/target/debug/deps/day04-57736d1bc1164831.rmeta: src/main.rs
-
-/Users/maarten/Documents/Code/adventofcode/2020/day04/target/debug/deps/day04-57736d1bc1164831.d: src/main.rs
-
-src/main.rs:
