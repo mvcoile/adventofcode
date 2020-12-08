966d892a8c6daa589a9ec21e945722d52a954821
Maarten Van Coile <maarten.vancoile@me.com>
2020-12-08 23:25:17 +0100

    day 3, 2020; base

---
+++ 2020/day03/src/main.rs
@@ -0,0 +1,14 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input_file = File::open("input")?;
+    let mut input = String::new();
+    input_file.read_to_string(&mut input)?;
+    let v_input: Vec<&str> = input.trim().split('\n').collect();
+
+    println!("{:?}", v_input);
+
+    Ok(())
+}
